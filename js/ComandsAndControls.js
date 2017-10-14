f6256c2231d91358de4dadd9433e0f30424580a6
blackswanny <blackswanny@gmail.com>
2017-10-14 12:38:55 -0700

    add init commands

--- js/ComandsAndControls.js
+++ js/ComandsAndControls.js
@@ -1,3 +1,16 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-    console.log("DOM fully loaded and parsed");
-});
+(function (document, window, undefined) {
+    window.SIA_COMMANDS = {
+        FORWARD: 'FORWARD',
+        BACK: 'BACK',
+        LEFT: 'LEFT',
+        RIGHT: 'RIGHT',
+        DOWN: 'DOWN',
+        UP: 'UP'
+    };
+    document.addEventListener("DOMContentLoaded", function(event) {
+
+
+
+
+    });
+})(document, window);
