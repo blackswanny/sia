646cbf90b3ba5adbeb90853bc4865a70fd4aa8ff
Sergiy Osypov <sergioosipov@gmail.com>
2017-10-14 18:10:06 -0700

    fix Info

--- js/Info.js
+++ js/Info.js
@@ -1,17 +1,25 @@
 (function (document, window, undefined) {
 
 
-    function showPopupModal() {
-
+    function showPopupModal(type) {
+alert(type);
+        var left = document.getElementById('left');
+        var right = document.getElementById('right');
+console.log("left", left);        
+        left.innerHTML = type;
+        right.innerHTML = type;
+        
+        left.style.display = block;
+        right.style.display = block;
     }
 
     document.addEventListener("DOMContentLoaded", function(event) {
-        window.addEventListener(SIA_COMMANDS.ARMREST, function(event) {});
-        window.addEventListener(SIA_COMMANDS.CUSHION, function(event) {});
-        window.addEventListener(SIA_COMMANDS.SUSPENSION, function(event) {});
-        window.addEventListener(SIA_COMMANDS.SAFETY_BELT, function(event) {});
-        window.addEventListener(SIA_COMMANDS.BENCH_SEAT, function(event) {});
-        window.addEventListener(SIA_COMMANDS.BACK_REST, function(event) {});
+        window.addEventListener(SIA_COMMANDS.ARMREST, function(event) { showPopupModal(SIA_COMMANDS.ARMREST); });
+        window.addEventListener(SIA_COMMANDS.CUSHION, function(event) { showPopupModal(SIA_COMMANDS.CUSHION); });
+        window.addEventListener(SIA_COMMANDS.SUSPENSION, function(event) { showPopupModal(SIA_COMMANDS.SUSPENSION); });
+        window.addEventListener(SIA_COMMANDS.SAFETY_BELT, function(event) { showPopupModal(SIA_COMMANDS.SAFETY_BELT); });
+        window.addEventListener(SIA_COMMANDS.BENCH_SEAT, function(event) { showPopupModal(SIA_COMMANDS.BENCH_SEAT); });
+        window.addEventListener(SIA_COMMANDS.BACK_REST, function(event) { showPopupModal(SIA_COMMANDS.BACK_REST); });
     });
 
 })(document, window);
